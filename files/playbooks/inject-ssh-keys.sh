5967e9e6df9c3e6546cc90937cd10573cdbcb948
Alex Schultz <aschultz@redhat.com>
2022-01-04 13:25:26 -0700

    Reduce ansible usage in containers
    Installing ansible in some of the containers to do some basic setup is
    very time and cpu consuming. This change switches the playbooks for ssh
    keys and waiting for gerrit & mysql to use a bash script rather than
    ansible.  The gerrit config is still performed via ansible so the
    timeouts needed to be raised in comparison to the ansible playbooks as
    the shell scripts are invoked much sooner than the playbooks were due to
    ansible pip install time.
    
    Additionally adds a variable (`podman_pull`) to skip the podman pull
    invocation before the pod create.
    
    Change-Id: I926fa8629b9d8d9286f0d78e461dd3376745a92b

---
+++ files/playbooks/inject-ssh-keys.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+set -ex
+
+mkdir -p /var/ssh
+chmod 0644 /var/ssh
+for I in id_rsa upstream_gerrit_key rdo_gerrit_key; do
+    set +x
+    key_cmd="echo \$$I | base64 -d | tee /var/ssh/$I"
+    eval $key_cmd
+    set -x
+    # add trailing line break required for the key file to be vaild. it gets
+    # eaten in the base64 conversion along the way
+    echo >> /var/ssh/$I
+    chmod 0600 /var/ssh/$I
+done
+ssh-keygen -y -f /var/ssh/id_rsa > /var/ssh/id_rsa.pub
+chmod 0600 /var/ssh/id_rsa.pub
