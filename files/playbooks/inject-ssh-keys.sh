871efc07591ea00666a8091f154a6075078a29b5
Bohdan Dobrelia <bdobreli@redhat.com>
2023-02-22 17:10:51 +0100

    Switch to user_priv_key instead of id_rsa
    Allow custom user keys, like those for id_ed25519
    
    Change-Id: Icaea4e7f14ba79f3bd9feba71a03f0d1b5414115
    Signed-off-by: Bohdan Dobrelia <bdobreli@redhat.com>

--- files/playbooks/inject-ssh-keys.sh
+++ files/playbooks/inject-ssh-keys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
+set -eux
 
 mkdir -p /var/ssh
 chmod 0644 /var/ssh
-for I in id_rsa upstream_gerrit_key rdo_gerrit_key; do
+for I in "$privkey" upstream_gerrit_key rdo_gerrit_key; do
     set +x
     key_cmd="echo \$$I | base64 -d | tee /var/ssh/$I"
     eval $key_cmd > /dev/null
@@ -13,6 +13,6 @@ for I in id_rsa upstream_gerrit_key rdo_gerrit_key; do
     echo >> /var/ssh/$I
     chmod 0600 /var/ssh/$I
 done
-ssh-keygen -y -f /var/ssh/id_rsa > /var/ssh/id_rsa.pub
-chmod 0600 /var/ssh/id_rsa.pub
+ssh-keygen -y -f "/var/ssh/$privkey" > "/var/ssh/$privkey.pub"
+chmod 0600 "/var/ssh/$privkey.pub"
 echo "Keys were injected"
